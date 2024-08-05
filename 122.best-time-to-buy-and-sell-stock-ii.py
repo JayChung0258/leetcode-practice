bb9dcddc5fc0bd12d21bd5e52216a39f15a9a52d
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2024-08-05 18:07:31 +0800

    Update 122.best-time-to-buy-and-sell-stock-ii.py

--- 122.best-time-to-buy-and-sell-stock-ii.py
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -9,7 +9,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         # 2 ptrs + Greedy
+        # if next price is lower than now, make profit once
+
+        prices.append(0)
         
+        res = 0
+        low = prices[0]
+        for i in range(len(prices)-1):
+            if prices[i+1] < prices[i]:
+                # cal profit
+                res += prices[i]-low
+                low = prices[i+1]
+
+        return res
+
 
 
 
